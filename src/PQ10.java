79bbce54f0a91d4c4d90f85287732de82842ea8a
jyoti <jyoti@lipun>
2024-03-11 22:37:48 +0530

    commit

--- src/PQ10.java
+++ src/PQ10.java
@@ -1,4 +1,10 @@
+//:-Print the number of rows in the given two-dimensional array, or the length of the outer array. Then print the number of columns, or the length of each inner array.
 
+//Ex. The array { {“hello”,”there”,”world”},{“how”,”are”,”you”} } should print:
+
+//Rows: 2
+
+//Columns: 3
 public class PQ10 {
 
 	public static void main(String[] args) {
